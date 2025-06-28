a6080bf93bdfe7984b49563ed364a76a47f03fd4
Luciano Mendes <104874429+Kamado8421@users.noreply.github.com>
2025-06-28 19:32:39 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="w-full h-full flex flex-col items-center">
       <h1 className="font-bold mt-20 mb-5 text-4xl md:text-5xl">
-        <span className="tag">&copy;</span> M<span className="tag">'</span>DEV <span className="tag">APIs</span> REST
+        <span className="tag">&copy;</span> M<span className="tag">'</span>DEVs <span className="tag">APIs</span> REST
       </h1>
       <span className="text-center mb-20 max-w-[300px] md:max-w-[100%]">
         Explore nossos serviços para Bots de WhatsApp. Entre em uma <br /> conta gratuitamente.
